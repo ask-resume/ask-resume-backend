53a40eb19cd09124c9b02810218122bcf258e591
m-dzn <mdzn.dev@gmail.com>
2023-05-29 22:51:31 +0900

    refactor(member): Migrate from Java to Kotlin

---
+++ src/main/java/app/askresume/domain/member/constant/MemberType.kt
@@ -0,0 +1,19 @@
+package app.askresume.domain.member.constant
+
+import java.util.*
+
+enum class MemberType {
+
+    LOCAL, GOOGLE;
+
+    fun from(type: String): MemberType {
+        return valueOf(type.uppercase(Locale.getDefault()))
+    }
+
+    fun isMemberType(type: String): Boolean {
+        val memberTypes = Arrays.stream(values())
+            .filter { memberType -> memberType.name.equals(type, ignoreCase = true) }.toList()
+        return memberTypes.size != 0
+    }
+
+}
---
+++ src/main/java/app/askresume/domain/member/repository/MemberRepository.kt
@@ -0,0 +1,14 @@
+package app.askresume.domain.member.repository
+
+import app.askresume.domain.member.constant.MemberType
+import app.askresume.domain.member.model.Member
+import org.springframework.data.jpa.repository.JpaRepository
+
+interface MemberRepository : JpaRepository<Member?, Long?> {
+
+    fun findByEmailAndMemberType(email: String, memberType: MemberType): Member?
+
+    fun findByRefreshToken(refreshToken: String): Member?
+
+    fun findMemberByEmailAndPasswordAndMemberType(email: String, password: String, memberType: MemberType): Member?
+}
