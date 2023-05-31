5c75d17f373df543514ce6a85e49d172c2c91388
Igor <132262b@naver.com>
2023-05-31 22:08:16 +0900

    refactor: enum code change #20

--- src/main/kotlin/app/askresume/domain/locale/constant/LocaleType.kt
+++ src/main/kotlin/app/askresume/domain/locale/constant/LocaleType.kt
@@ -1,22 +1,23 @@
 package app.askresume.domain.locale.constant
 
-import java.util.*
 
-enum class LocaleType(private val value: String) {
+enum class LocaleType(
+    val value: String
+) {
 
-    KO("Korean"), EN("English");
+    KO("Korean"),
+    EN("English"),
+    ;
 
     fun value() = value
 
     companion object {
         fun from(type: String): LocaleType {
-            return valueOf(type.uppercase(Locale.getDefault()))
+            return LocaleType.valueOf(type.uppercase())
         }
 
         fun isLocaleType(type: String): Boolean {
-            val localeTypes = Arrays.stream(values())
-                .filter { localeType -> localeType.value.equals(type, ignoreCase = true) }.toList()
-            return localeTypes.size != 0
+            return LocaleType.values().any { it.name == type.uppercase() }
         }
     }
 }
