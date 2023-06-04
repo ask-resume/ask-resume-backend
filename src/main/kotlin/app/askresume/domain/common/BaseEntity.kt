8608c68dece94e138e7d5180f1e892123ce4075a
Igor <132262b@naver.com>
2023-06-04 23:58:05 +0900

    refactor: baseEntity modification

--- src/main/kotlin/app/askresume/domain/common/BaseEntity.kt
+++ src/main/kotlin/app/askresume/domain/common/BaseEntity.kt
@@ -1,7 +1,9 @@
 package app.askresume.domain.common
 
+import org.hibernate.annotations.Comment
 import org.springframework.data.annotation.CreatedBy
 import org.springframework.data.annotation.LastModifiedBy
+import org.springframework.data.annotation.LastModifiedDate
 import org.springframework.data.jpa.domain.support.AuditingEntityListener
 import javax.persistence.Column
 import javax.persistence.EntityListeners
@@ -10,11 +12,15 @@ import javax.persistence.MappedSuperclass
 @EntityListeners(value = [AuditingEntityListener::class])
 @MappedSuperclass
 abstract class BaseEntity : BaseTimeEntity() {
+
+    @Comment("등록자")
     @CreatedBy
     @Column(updatable = false)
     val createdBy: String? = null
 
+    @Comment("수정자")
     @LastModifiedBy
+    @Column(insertable = false)
     val updatedBy: String? = null
 }
 
