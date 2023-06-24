e7826004906164797794ad1389ef2efe10a0fd5d
Igor <132262b@naver.com>
2023-06-24 14:35:16 +0900

    style: facade style change

--- src/main/kotlin/app/askresume/api/job/facade/JobFacade.kt
+++ src/main/kotlin/app/askresume/api/job/facade/JobFacade.kt
@@ -3,9 +3,10 @@ package app.askresume.api.job.facade
 import app.askresume.api.job.dto.response.JobResponse
 import app.askresume.domain.job.service.JobService
 import app.askresume.domain.locale.constant.LocaleType
+import app.askresume.global.annotation.Facade
 import org.springframework.stereotype.Service
 
-@Service
+@Facade
 class JobFacade(
     private val jobService: JobService,
 ) {
