c30223d5a863b862677f299bdc7dedb486a2c371
Igor <132262b@naver.com>
2023-06-27 23:31:13 +0900

    feat: I have developed a domain for storing result data (#56)

---
+++ src/main/kotlin/app/askresume/domain/result/repository/ResultRepository.kt
@@ -0,0 +1,7 @@
+package app.askresume.domain.result.repository;
+
+import app.askresume.domain.result.model.Result
+import org.springframework.data.jpa.repository.JpaRepository
+
+interface ResultRepository : JpaRepository<Result, Long> {
+}
---
+++ src/main/kotlin/app/askresume/domain/result/service/ResultService.kt
@@ -0,0 +1,9 @@
+package app.askresume.domain.result.service
+
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+
+@Service
+@Transactional(readOnly = true)
+class ResultService {
+}
