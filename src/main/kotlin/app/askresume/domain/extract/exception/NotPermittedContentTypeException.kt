b17db6cca7a822b73d1e3bbf0e6315cc78efa62c
Igor <132262b@naver.com>
2023-07-30 22:12:18 +0900

    fix: error code가 잘못 출력되는 문제 수정

--- src/main/kotlin/app/askresume/domain/extract/exception/NotPermittedContentTypeException.kt
+++ src/main/kotlin/app/askresume/domain/extract/exception/NotPermittedContentTypeException.kt
@@ -6,7 +6,7 @@ import app.askresume.global.error.exception.BusinessException
 open class NotPermittedContentTypeException(
     override val cause: Throwable? = null,
 ) : BusinessException(
-    codeBook = ErrorCodes.ENTITY_NOT_FOUND,
+    codeBook = ErrorCodes.NOT_PERMITTED_CONTENT_TYPE,
     properties = "not.permitted.content.type",
     arguments = arrayOf(),
     cause = cause,
