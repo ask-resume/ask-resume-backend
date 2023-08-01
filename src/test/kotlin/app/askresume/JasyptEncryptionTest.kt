30712c490a5872e52e98fd8aabcd62220f3f740c
Igor <132262b@naver.com>
2023-08-01 21:32:44 +0900

    test: JasyptEncryptionTest에 상수를 companion object로 변경

--- src/test/kotlin/app/askresume/JasyptEncryptionTest.kt
+++ src/test/kotlin/app/askresume/JasyptEncryptionTest.kt
@@ -5,22 +5,23 @@ import org.junit.jupiter.api.Test
 
 // 해당 부분 내용 추가 후 Commit 금지
 class JasyptEncryptionTest {
-
-    val ALGORITHM_NAME: String = "PBEWithMD5AndTripleDES"
-
     @Test
     fun jasyptTest() {
         val password = "1111" // 아무거나 적어놔야 test 됨
         val encryptor = PooledPBEStringEncryptor()
         encryptor.setPoolSize(4)
         encryptor.setPassword(password)
-        encryptor.setAlgorithm(ALGORITHM_NAME)
+        encryptor.setAlgorithm(Companion.ALGORITHM_NAME)
 
-        val content = "ask_resume_prod"    // 암호화 할 내용
+        val content = "ask_resume"    // 암호화 할 내용
         val encryptedContent = encryptor.encrypt(content) // 암호화
         val decryptedContent = encryptor.decrypt(encryptedContent) // 복호화
 
         println("Enc : $encryptedContent")
         println("Dec: $decryptedContent")
+    }
+
+    companion object {
+        private const val ALGORITHM_NAME: String = "PBEWithMD5AndTripleDES"
     }
 }
