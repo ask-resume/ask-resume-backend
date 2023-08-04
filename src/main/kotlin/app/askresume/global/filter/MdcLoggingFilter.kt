e855c588f4009f3138f80ca02babd07c39f2e380
Igor <132262b@naver.com>
2023-08-05 00:26:10 +0900

    fix: ip를 가져오는 코드를 header에서 가져오도록 수정

--- src/main/kotlin/app/askresume/global/filter/MdcLoggingFilter.kt
+++ src/main/kotlin/app/askresume/global/filter/MdcLoggingFilter.kt
@@ -24,13 +24,29 @@ class MdcLoggingFilter : Filter {
 
         val httpServletRequest = request as HttpServletRequest
 
-        log.info("Incoming Request - TraceId: $uuid, IP Address: ${request.remoteAddr}, API Endpoint: [${httpServletRequest.method}]${httpServletRequest.requestURI}")
+        log.info("Incoming Request - TraceId: $uuid, IP Address: ${getClientIP(request)}, API Endpoint: [${httpServletRequest.method}]${httpServletRequest.requestURI}")
         chain.doFilter(request, response)
         MDC.clear()
     }
 
+    fun getClientIP(request: HttpServletRequest): String {
+        var ip = request.getHeader(X_FORWARDED_FOR)
+        if (ip == null) ip = request.getHeader(PROXY_CLIENT_IP)
+        if (ip == null) ip = request.getHeader(WL_PROXY_CLIENT_IP)
+        if (ip == null) ip = request.getHeader(HTTP_CLIENT_IP)
+        if (ip == null) ip = request.getHeader(HTTP_X_FORWARDED_FOR)
+        if (ip == null) ip = request.remoteAddr
+
+        return ip
+    }
+
     companion object {
-        const val TRACE_ID = "traceId"
+        private const val TRACE_ID = "traceId"
+        private const val X_FORWARDED_FOR = "X-Forwarded-For"
+        private const val PROXY_CLIENT_IP = "Proxy-Client-IP"
+        private const val WL_PROXY_CLIENT_IP = "WL-Proxy-Client-IP"
+        private const val HTTP_CLIENT_IP = "HTTP_CLIENT_IP"
+        private const val HTTP_X_FORWARDED_FOR = "HTTP_X_FORWARDED_FOR"
     }
 
 }
