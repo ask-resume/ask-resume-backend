802004f7a3480c8aeb55b386a650e8d3f8053aef
Igor <132262b@naver.com>
2023-08-14 22:28:23 +0900

    docs: job vo api docs 작업

--- src/main/kotlin/app/askresume/api/job/vo/AdminJobVo.kt
+++ src/main/kotlin/app/askresume/api/job/vo/AdminJobVo.kt
@@ -2,14 +2,17 @@ package app.askresume.api.job.vo
 
 import io.swagger.v3.oas.annotations.media.Schema
 import org.hibernate.validator.constraints.Length
+import javax.validation.constraints.NotBlank
 
 @Schema(description = "어드민 직업정보 등록 Request")
 data class SaveJobRequest(
+    @field:NotBlank
     @field:Length(max = 150)
-    @field:Schema(description = "직업 영어명", example = "backend developer")
+    @field:Schema(description = "직업 영어명", example = "backend developer", required = true)
     val englishJobName: String,
 
+    @field:NotBlank
     @field:Length(max = 150)
-    @field:Schema(description = "직업 한글명", example = "백엔드 개발자")
+    @field:Schema(description = "직업 한글명", example = "백엔드 개발자", required = true)
     val koreaJobName: String,
 )
