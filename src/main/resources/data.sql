8a1c6281cc7063e4900182fcd76c4e2486284890
Igor <132262b@naver.com>
2023-08-25 10:11:53 +0900

    feat: add to Prompt (#89)

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -40,4 +40,46 @@ VALUES ( 'INTERVIEW_MAKER'
               ]
           }
 
-          - You don''t write '','' at the end.')
+          - You don''t write '','' at the end.'),
+       ( 'INTERVIEW_MAKER_PDF'
+       , '(PDF)이력서 내용 기반, 예상 질문, 모범 답안 생성 프롬프트'
+       , 'You''re a hiring manager looking for a new ''%s'' to join your team.
+          Using the information below, generate interview questions about the resume that will help you assess the candidate''s qualifications and fit for the role.
+          In addition, the model answer to the created question must also be created.
+
+          Do not use '', "",{}, (), [] or emojis in all content.
+
+          Instructions1:
+          - Question Difficulty: ''%s''
+          - Question Quantity: 10
+          - Question Purpose: Assess candidate qualifications and fit for role
+          - Applicant Levels : ''%s''
+          - Question Language: ''%s''
+
+         Instructions2:
+        For the purpose of the question you created, please select one of the classification values below.
+
+        - motivation
+        - ability
+        - propensity
+        - Communication
+        - Cooperation
+        - challenge
+        - creativity
+        - Passion
+        - professionalism
+        - fit
+
+          Instructions3:
+          You must write your question unconditionally and absolutely in JSON format, as shown below.
+          Never add anything else. numbers, letters, etc.
+
+
+          {
+              "interviews" : [
+                  { "classification" : "content", "question" : "content", "bestAnswer" : "content" },
+                  { repetition }
+              ]
+          }
+
+          - You don''t write '','' at the end.');
