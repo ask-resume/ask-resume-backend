2c408d891c9bb0bafd87a12906aa8d4bfa13a81f
Igor <132262b@naver.com>
2024-02-11 02:20:56 +0900

    test: add to jobReadOnlyServiceTest

---
+++ src/test/kotlin/app/askresume/domain/job/service/JobReadOnlyServiceTest.kt
@@ -0,0 +1,70 @@
+package app.askresume.domain.job.service
+
+import app.askresume.RANDOM
+import app.askresume.domain.job.dto.JobDto
+import app.askresume.domain.job.model.JobMaster
+import app.askresume.domain.job.repository.JobDataRepositoryQuery
+import app.askresume.domain.job.repository.JobMasterRepository
+import app.askresume.domain.locale.constant.LocaleType
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.mockito.BDDMockito.given
+import org.mockito.BDDMockito.then
+import org.mockito.InjectMocks
+import org.mockito.Mock
+import org.mockito.junit.jupiter.MockitoExtension
+import java.util.*
+
+@ExtendWith(MockitoExtension::class)
+class JobReadOnlyServiceTest {
+
+    @Mock
+    private lateinit var jobDataRepositoryQuery : JobDataRepositoryQuery
+
+    @Mock
+    private lateinit var jobMasterRepository: JobMasterRepository
+
+    @InjectMocks
+    private lateinit var jobReadOnlyService : JobReadOnlyService
+
+    @DisplayName("직업 ID로 이용해, 직업 고유명을 조회한다.")
+    @Test
+    fun findJobMasterNameTest() {
+        // given
+        val jobMasterId = RANDOM.nextLong()
+
+        val jobMaster = RANDOM.nextObject(JobMaster::class)
+
+        given(jobMasterRepository.findById(jobMasterId)).willReturn(Optional.of(jobMaster))
+
+        // when
+        val selectJobMaster = jobReadOnlyService.findJobMasterName(jobMasterId)
+
+        // then
+        then(jobMasterRepository).should().findById(jobMasterId)
+        assertThat(selectJobMaster).isEqualTo(jobMaster.masterName)
+    }
+
+
+    @DisplayName("locale 정보로 해당 언어의 직업을 조회한다.")
+    @Test
+    fun findJobsTest() {
+        // given
+        val localeType = RANDOM.nextObject(LocaleType::class)
+
+        val list = RANDOM.nextList(JobDto::class)
+        given(jobDataRepositoryQuery.findJobs(localeType)).willReturn(list)
+
+        // when
+        jobReadOnlyService.findJobs(localeType)
+
+        // then
+        then(jobDataRepositoryQuery).should().findJobs(localeType)
+    }
+
+}
+
+
+
