6e01f81f2d8c5185d0e0ec4044f06ce9a388b88f
Igor <132262b@naver.com>
2024-02-10 21:17:46 +0900

    test: random  util improvement

--- src/test/kotlin/app/askresume/RANDOM.kt
+++ src/test/kotlin/app/askresume/RANDOM.kt
@@ -7,9 +7,7 @@ import kotlin.reflect.KClass
 
 
 object RANDOM {
-    fun nextString() = this.nextString(DEFAULT_STRING_MIN_LENGTH, DEFAULT_STRING_MAX_LENGTH)
-
-    fun nextString(minSize: Int, maxSize: Int): String = EasyRandomParameters()
+    fun nextString(minSize: Int = 1, maxSize: Int = 1000): String = EasyRandomParameters()
         .stringLengthRange(minSize, maxSize)
         .seed(Random().nextLong(Long.MAX_VALUE))
         .let {
@@ -19,23 +17,31 @@ object RANDOM {
     fun nextEmail(): String =
         "${nextString(EMAIL_MIN_LENGTH, EMAIL_MAX_LENGTH)}${TEST_DOMAIN}"
 
+    fun nextInt(
+        minSize: Int = 0,
+        maxSize: Int = Int.MAX_VALUE
+    ): Int = Random().nextInt(minSize, maxSize)
 
-    fun nextInt() = this.nextInt(0, Int.MAX_VALUE)
+    fun nextLong(
+        minSize: Long = 0,
+        maxSize: Long = Long.MAX_VALUE
+    ): Long = Random().nextLong(minSize, maxSize)
 
-    fun nextInt(minSize: Int, maxSize: Int): Int = Random().nextInt(minSize, maxSize)
-
-    fun nextLong() = this.nextLong(0, Long.MAX_VALUE)
-    fun nextLong(minSize: Long, maxSize: Long): Long = Random().nextLong(minSize, maxSize)
-
-    fun <T : Any> nextObject(kClass: KClass<T>): T {
+    fun <T : Any> nextObject(`class`: KClass<T>): T {
         val easyRandom = EasyRandom(EasyRandomParameters())
-        val javaClass = kClass.java
-        return easyRandom.nextObject(javaClass)
+        return easyRandom.nextObject(`class`.java)
     }
 
+    fun <T : Any> nextList(`class`: KClass<T>, size: Int = 10): List<T> {
+        val easyRandom = EasyRandom(EasyRandomParameters())
+        val mutableList = mutableListOf<T>()
 
-    private const val DEFAULT_STRING_MIN_LENGTH = 1
-    private const val DEFAULT_STRING_MAX_LENGTH = 1000
+        repeat(size) {
+            val randomObject = easyRandom.nextObject(`class`.java)
+            mutableList.add(randomObject)
+        }
+        return mutableList.toList()
+    }
 
     private const val EMAIL_MIN_LENGTH = 5
     private const val EMAIL_MAX_LENGTH = 15
