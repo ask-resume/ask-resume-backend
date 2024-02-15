353eb9e1610e976c0808b1723a0bb7d4ec4e99aa
Igor <132262b@naver.com>
2024-02-15 23:39:00 +0900

    test: submitCommandServiceTest

---
+++ src/test/kotlin/app/askresume/domain/submit/service/SubmitCommandServiceTest.kt
@@ -0,0 +1,53 @@
+package app.askresume.domain.submit.service
+
+import app.askresume.RANDOM
+import app.askresume.domain.member.repository.MemberRepository
+import app.askresume.domain.submit.constant.SubmitStatus
+import app.askresume.domain.submit.model.Submit
+import app.askresume.domain.submit.repository.SubmitDataRepository
+import app.askresume.domain.submit.repository.SubmitRepository
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.mockito.BDDMockito.given
+import org.mockito.BDDMockito.then
+import org.mockito.InjectMocks
+import org.mockito.Mock
+import org.mockito.junit.jupiter.MockitoExtension
+import java.util.*
+
+
+@ExtendWith(MockitoExtension::class)
+class SubmitCommandServiceTest {
+
+    @Mock
+    private lateinit var memberRepository: MemberRepository
+
+    @Mock
+    private lateinit var submitRepository: SubmitRepository
+
+    @Mock
+    private lateinit var submitDataRepository: SubmitDataRepository
+
+    @InjectMocks
+    private lateinit var submitCommandService: SubmitCommandService
+
+
+    @DisplayName("제출건의 상태를 변경한다.")
+    @Test
+    fun updateStatusTest() {
+        // given
+        val submitId = RANDOM.nextLong()
+        val changeStatus = RANDOM.nextObject(SubmitStatus::class)
+
+        val submit = RANDOM.nextObject(Submit::class)
+        given(submitRepository.findById(submitId)).willReturn(Optional.of(submit))
+
+        // when
+        submitCommandService.updateStatus(submitId, changeStatus)
+
+        // then
+        then(submitRepository).should().findById(submitId)
+    }
+
+}
