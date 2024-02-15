9f2bf7f2868f463e9e72cc4aca3bcf8d70d97ba4
Igor <132262b@naver.com>
2024-02-15 23:37:47 +0900

    test: add to mockk example

---
+++ src/test/kotlin/app/askresume/domain/job/service/JobReadOnlyServiceMockkOnlyTest.kt
@@ -0,0 +1,50 @@
+package app.askresume.domain.job.service
+
+import app.askresume.RANDOM
+import app.askresume.domain.job.model.JobMaster
+import app.askresume.domain.job.repository.JobDataRepositoryQuery
+import app.askresume.domain.job.repository.JobMasterRepository
+import app.askresume.domain.job.repository.findJobMasterById
+import io.mockk.every
+import io.mockk.impl.annotations.InjectMockKs
+import io.mockk.impl.annotations.MockK
+import io.mockk.junit5.MockKExtension
+import io.mockk.verify
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+
+@ExtendWith(MockKExtension::class)
+class JobReadOnlyServiceMockkOnlyTest {
+
+    @MockK
+    private lateinit var jobMasterRepository: JobMasterRepository
+
+    @MockK
+    private lateinit var jobDataRepositoryQuery: JobDataRepositoryQuery
+
+    @InjectMockKs
+    private lateinit var jobReadOnlyService: JobReadOnlyService
+
+    @DisplayName("직업 ID로 이용해, 직업 고유명을 조회한다.")
+    @Test
+    fun findJobMasterNameTest() {
+        // given
+        val jobMasterId = RANDOM.nextLong()
+        val jobMaster = RANDOM.nextObject(JobMaster::class)
+
+        every { jobMasterRepository.findJobMasterById(jobMasterId) } returns (jobMaster)
+
+        // when
+        val selectJobMaster = jobReadOnlyService.findJobMasterName(jobMasterId)
+
+        // then
+        verify(exactly = 1) { jobMasterRepository.findJobMasterById(jobMasterId) }
+        assertThat(selectJobMaster).isEqualTo(jobMaster.masterName)
+    }
+
+}
+
+
+
